ecd45a5c0ce655e69a5d2c44276590acafded420
arunkumarae <arunkumar.ae45@gmail.com>
2026-04-29 09:17:51 +0530

    CORS Error fix

--- src/main/java/com/line/business/work/configuration/CorsConfig.java
+++ src/main/java/com/line/business/work/configuration/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
 
 	    CorsConfiguration config = new CorsConfiguration();
 
-	    config.setAllowedOrigins(List.of("http://localhost:5173"));
+	    config.setAllowedOrigins(List.of("*"));
 	    config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
 	    config.setAllowedHeaders(List.of("*"));
 	    config.setAllowCredentials(true);
